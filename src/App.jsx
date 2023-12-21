1cc4769d2f968130d4423edf173a4b14dd857ab8
Thomas Crowe <wtcrowe4@outlook.com>
2023-12-21 07:50:07 -0500

    started pages/navbar routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,47 @@ import './App.css'
 function App() {
   
   return (
-    <>
-    <main className="bg-slate-300/20">
+    <div className='bg-slate-300/20'>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>  
-    </main>
+        {/* <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes> */}
+      </Router>
+    </div>
     
-
-    
-      {/* <h1 class='text-5xl font-bold underline p-5'>Thomas Crowe</h1>
-      <h2 class='text-2xl p-5'>Full Stack Developer</h2>
-
-      <div class='flex flex-row justify-center items-center'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-    </>
   )
 }
 
 export default App
+// <div className='bg-slate-300/20'>
+    // <Router>
+    // <Navbar />
+    // <div>
+    //   <h1 class='text-5xl font-bold underline p-5'>Thomas Crowe</h1>
+    //   <h2 class='text-2xl p-5'>Full Stack Developer</h2>
+
+    //   <div class='flex flex-row justify-center items-center'>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    // </div> 
+    // </Router>
+    // </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import { NavLink } from "react-router-dom"
+import viteLogo from '/vite.svg'
 
 const Navbar = () => {
   return (
-    <header className="header">
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-            <p className='blue-gradient-_text text-2xl'>Thomas</p>
+    <header className='header'>
+      <NavLink to='/'>
+        <img src={viteLogo} alt='logo' className='w-18 h-18 object-contain' />
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
         </NavLink>
-
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+        <NavLink to='/contact' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Contact
+        </NavLink>
+      </nav>
     </header>
   )
 }
